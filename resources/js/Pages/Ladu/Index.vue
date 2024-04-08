f745d70ae9a6b977e6ffbd1abaca501f216c8c88
timoloho <92794268+timoloho@users.noreply.github.com>
2024-04-08 13:14:29 +0300

    uwu

--- resources/js/Pages/Ladu/Index.vue
+++ resources/js/Pages/Ladu/Index.vue
@@ -14,7 +14,7 @@ defineProps({
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Ladu</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Ladu puuks peer</h2>
         </template>
 
         <div class="py-12">
