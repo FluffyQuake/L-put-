ce43cd31dbb54f5c3824279aace3854d03a8ab90
Kaspar Vakkum <apelsin.kv@gmail.com>
2024-04-10 13:08:31 +0300

    Logo change, navbar change

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,4 +1,7 @@
 <template>
-    <img src="/storage/coop.png"/>
+    <div class="w-52 h-14 mt-4 ">
+        <img src="/storage/coop.png"/>
+
+    </div>
     <!-- <img src="{{ ('storage/images/'.$article->image) }}" alt="coop logo" title="" />     -->
 </template>
